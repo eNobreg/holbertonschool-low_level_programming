b631874617886ddf67a0ba190c32a29711d0ac7f
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-17 06:21:17 -0400

    Puts string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Is simply using puts to print a string
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
