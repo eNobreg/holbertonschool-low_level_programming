da27f0e784874d9b2044e38ae2bd7e7024c925b8
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-17 16:25:59 -0400

    First if states

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastdig = n % 10;
 
-	if (lastdigit > 5)
+	if (lastdig > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
-	else if (lastdigit < 6 && lastdig != 0)
+	else if (lastdig < 6 && lastdig != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
 	else
 		printf("Last digit of %d is %d and is 0\n", n, lastdig);
