0eae016f9f598b008a18d233e72f2b62d767e4f9
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-17 16:16:20 -0400

    removed space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	if (lastdigit > 5)
 		printf(" and is greater than 5\n");
 	else if (lastdigit == 0)
-		 printf(" and is 0\n");
+		printf(" and is 0\n");
 	else
 		printf(" and is less than 6 and not 0\n");
 	return (0);
