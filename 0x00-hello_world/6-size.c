c01f6b5008e837a8115cb1610db1d75f281c7e43
eNobreg <ezra.nobrega@outlook.com>
2019-09-17 07:28:36 -0400

    Size of file types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,12 @@
  * main - print f is printing the size of a type using sizeof
  * Return: 0 always
  */
-
 int main(void)
 {
 	printf("Size of a char %li byte(s)\n", sizeof(char));
 	printf("Size of an int %li byte(s)\n", sizeof(int));
-	printf("Size of a long int %li byte(s)\n", sizeof(long));
-	printf("Size of a long long int %li byte(s)\n", sizeof(long long));
+	printf("Size of a long int %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int %li byte(s)\n", sizeof(long long int));
 	printf("Size of a float %li byte(s)\n", sizeof(float));
 	return (0);
 }
