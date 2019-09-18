dcbb88575ca281297c6ae4e78c256cfd1340ddb7
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 06:44:16 -0400

    Add things to make betty happy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,28 @@
 #include "holberton.h"
-
+/**
+ * times_table - prints the times tables
+ * Return: Never
+ */
 void times_table(void)
 {
 
 	int count, c2, result;
-	c2 = 0;
 
+	c2 = 0;
 	while (c2 < 10)
 	{
 		count = 0;
-		while(count < 10)
+		while (count < 10)
 		{
 			result = c2 * count;
 
-			if (result/10)
+			if (result / 10)
 			{
-				_putchar(result/10 + '0');
-				_putchar(result%10 + '0');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 			else
-				_putchar( result + '0');
+				_putchar(result + '0');
 
 			if (count != 9)
 			{
