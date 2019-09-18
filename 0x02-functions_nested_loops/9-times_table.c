45d720d5845c0c242bc1f58355206e5955aeb136
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 06:40:32 -0400

    Add new header, add new c program

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+void times_table(void)
+{
+
+	int count, c2, result;
+	c2 = 0;
+
+	while (c2 < 10)
+	{
+		count = 0;
+		while(count < 10)
+		{
+			result = c2 * count;
+
+			if (result/10)
+			{
+				_putchar(result/10 + '0');
+				_putchar(result%10 + '0');
+			}
+			else
+				_putchar( result + '0');
+
+			if (count != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			count++;
+		}
+	c2++;
+	_putchar('\n');
+	}
+}
+
