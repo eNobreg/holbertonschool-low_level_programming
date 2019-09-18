35b64e09a5ad8582ea08296878b744f4767f7ea4
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 09:29:39 -0400

    Add a lot of _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,18 @@
-#include <unistd.h>
+#include "holerton.h"
 
 
 int main(void)
 {	
-	write(1,"Holberton\n", 11);
+	_putchar('H');
+	_putchar('o');
+       	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
+	
 }
