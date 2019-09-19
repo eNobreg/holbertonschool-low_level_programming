4655f33e370004b6cdc087d22eba511574312e14
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 21:07:55 -0400

    Add seventh program, print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - Prints a line
+ * @n: length to print
+ * Return: Never
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar(95);
+	}
+	_putchar('\n');
+}
