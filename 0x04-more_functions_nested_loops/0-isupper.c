a03d9423224ddb68291b193c6ae1469893410174
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 22:04:31 -0400

    add if upper script

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isupper - checks if value is upper or lower
+ * @c: Value passed in
+ * Return: Always 1 0r 2
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
