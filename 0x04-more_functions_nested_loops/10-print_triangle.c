015a245a0d6ba4545d129a9642044f1870371fbb
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 22:59:40 -0400

    This file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,20 @@ void print_triangle(int size)
 {
 
 	int i, j;
+	i = 1;
 
-	if (size <= 0)
+	if (i != 0)
 	{
+		for (i = 1; i <= size ; i++)
+		{
+			for (j = i; j < size ; j++)
+				_putchar(32);
+			for (j = 1; j <= i ; j++)
+				_putchar(35);
+			if (i <= size)
+				_putchar('\n');
+		}
+	}
+	else
 		_putchar('\n');
-		return;
-	}
-	for (i = 0; i <= size ; i++)
-	{
-		for (j = i; j < size ; j++)
-			_putchar(32);
-		for (j = 1; j <= i ; j++)
-			_putchar(35);
-		if (i != size)
-			_putchar('\n');
-	}
 }
