5859259bd92151054620d6101ee19d6c3a19e536
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 21:59:53 -0400

    Multiplying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - multiplies a and b
+ * @a: Value to pass in
+ * @b: Value to pass in
+ * Return: Always A multiplied by B
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
