fc71643e9801f49926ff9ad11b3dab7e1d2b6106
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 20:48:04 -0400

    Add diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
-
+/**
+ * print_diagonal - Printing a diagonal line
+ * @n: value passed in
+ * Return: Never
+ */
 void print_diagonal(int n)
 {
 
 	int i, j;
-	for(i = 1; i <= n ; i++)
+
+	for (i = 1; i <= n ; i++)
 	{
-		for(j = 2; j <= i ; j++) 	
+		for (j = 2; j <= i ; j++)
 			_putchar(32);
 		_putchar(92);
 		_putchar('\n');
