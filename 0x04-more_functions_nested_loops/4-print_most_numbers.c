42f6294249d6769acd02fb56ce40b83be9741c44
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 21:55:20 -0400

    betty compliant

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
-
+/**
+ * print_most_numbers - Printing numbers except 2 and 4
+ * Return: Never
+ */
 
 void print_most_numbers(void)
 {
 	int i;
+
 	i = 0;
 	while (i < 10)
 	{
 		if (i != 2 && i != 4)
-		{	
+		{
 			_putchar(i + '0');
 		}
 		i++;
