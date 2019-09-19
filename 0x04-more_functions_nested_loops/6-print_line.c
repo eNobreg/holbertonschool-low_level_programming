9a79c7f1fd3b992d591a593c5bbe674dcf10fa33
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 21:46:51 -0400

    Add corrections

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,17 @@
  */
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		while (i < n)
+		{
 			_putchar(95);
-		_putchar('\n');
+			i++;
+		}
+
+			
 	}
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
