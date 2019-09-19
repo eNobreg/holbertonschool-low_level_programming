b2426ca759b391bbe0340b5dc1ceb49adf95f127
Ezra Nobrega <ezra.nobrega@outlook.com>
2019-09-18 20:25:48 -0400

    add fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - includes all the code for the program
+ * Return: Returns an int
+ */
+
+int main(void)
+{
+	int counter, sum;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		if ((counter % 5 == 0) && (counter % 3 == 0))
+			printf("FizzBuzz ");
+
+		else if (counter % 3 == 0)
+			printf("Fizz ");
+		else if (counter % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", counter);
+	}
+	printf("\n");
+	return (0);
+
+}
