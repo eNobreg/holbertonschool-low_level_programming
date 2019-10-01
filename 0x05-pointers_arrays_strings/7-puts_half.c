c25bff913f5e3c75e52a75c7d93efb3f65ac453e
eNobreg <ez@humdub.com>
2019-10-01 12:59:29 -0400

    Eigth program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+
+void puts_half(char *str)
+{
+	int length, n;
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		n = length/2;
+		while (n < length)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+
+	}
+	else
+	{
+		n = (length - 1) / 2;	
+		while (length >= n)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
