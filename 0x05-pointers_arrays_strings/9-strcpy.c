fc654ec578b2ce741bb3d5b6f1505470f01ade83
eNobreg <ez@humdub.com>
2019-10-01 14:26:10 -0400

    Updated with desc

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
-
+/**
+ * *_strcpy - Prints a string
+ * @dest: Destination
+ * @src: Source
+ * Return: Always dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
 	for(i = 0; src[i] != '\0'; i++ )
 	{
-		dest[i] = src[i]
+		dest[i] = src[i];
 	}
 	dest[i] = '\n';
 	return (dest);
