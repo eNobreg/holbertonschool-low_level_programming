246248109cce28ae07520edb412de238c6030251
root <root@Umbra.localdomain>
2019-10-01 10:05:45 -0400

    Made betty happy

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
-
+/**
+ * print_rev - Reverses given string
+ * _strlen - Gets stringth Length
+ * @s: Pointer for string
+ * Return: Never
+ */
 void print_rev(char *s)
 {
 	int len;
-	len = _strlen(s);
 
-	for (;(len - 2) >= 0;)
+	len = _strlen(s);
+	len--;
+
+	while (s[len] != '\0')
 	{
 		_putchar(s[len]);
 		len--;
@@ -13,6 +20,11 @@ void print_rev(char *s)
 	_putchar('\n');
 }
 
+/**
+ * _strlen - length function
+ * @s: character passed in
+ * Return: Always C
+ */
 int _strlen(char *s)
 {
 	int c = 0;
