57c7efb1b70c2115cc14de103cea45e95bb16734
eNobreg <ez@humdub.com>
2019-10-01 13:35:14 -0400

    Ninth programv2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * print_array - Prints an array based on input
+ * @a: Array passed in
+ * @n: Number of array entries to print
+ * Return: Never
+ */
 
 void print_array(int *a, int n)
 {
 	int count;
+
 	for (count = 0; count < n; count++)
 	{
-		if (count != n-1)
+		if (count != n - 1)
 			printf("%d, ", a[count]);
 		else
 			printf("%d", a[count]);
