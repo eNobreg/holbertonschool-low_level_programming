e29953c772f8eaf99d50d0c82988161385cbca94
root <root@Umbra.localdomain>
2019-10-01 09:46:16 -0400

    Adding function within

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,19 @@ void print_rev(char *s)
 	int len;
 	len = _strlen(s);
 
-	for (;(len - 1) >= 0;)
+	for (;(len - 2) >= 0;)
 	{
 		_putchar(s[len]);
 		len--;
 	}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
