5c8673e16c9639f51b54f80d170dfd8436328b91
eNobreg <ez@humdub.com>
2019-10-07 15:36:22 -0400

    update to header and new program

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets a string
+ * @s: String to set
+ * @to: thing to set to
+ * Return: Never
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
