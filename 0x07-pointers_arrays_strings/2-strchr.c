6a3ac570fd3c9fe99d712cbbf002e5ec0d5fd954
eNobreg <ez@humdub.com>
2019-10-07 17:19:22 -0400

    make betty compliant

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 /**
- * _memcpy - sets a portion of memory to a byte
- * @src: value passed in
- * @dest: character to fill memory with
- * @n: How many bytes to fill
+ * _strchr - sets a portion of memory to a byte
+ * @s: value passed in
+ * @c: character to fill memory with
  * Return: Always a pointer to s
  */
 char *_strchr(char *s, char c)
 {
 	while (s[0] != '\0')
 	{
-		if(s[0] == c)
-			return(s);
+		if (s[0] == c)
+			return (s);
 		else if (s[1] == c)
 			return (s + 1);
-		
 		s++;
 	}
 	return (s + 1);
