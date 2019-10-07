e5975c60c6652d87339a4ec401d648a88cd0ddfe
eNobreg <ez@humdub.com>
2019-10-07 15:16:08 -0400

    update header file, add program eight

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sums of diagonal numbers in a 2D array
+ * @a: Array passed into the function
+ * @size: Size of the array passed in
+ * Return: never
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(size + 1) * i];
+	}
+	for (j = 0 ; j < size; j++)
+	{
+		sum2 += a[(size - 1) * (j + 1)];
+	}
+
+
+	printf("%d, %d\n", sum1, sum2);
+}
