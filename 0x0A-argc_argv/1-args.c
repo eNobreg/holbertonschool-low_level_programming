3eed52beefa089f1b6e38ae7d017c277ad0d8cc6
eNobreg <ez@humdub.com>
2019-10-12 22:38:08 -0400

    header and update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Main function
+ * @argc: Count passed in
+ * @argv: Values passed in
+ * Return: Always 0
+ */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
