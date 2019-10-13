050b87e6ce20f4e71bd01249414f552f723c4b74
eNobreg <ez@humdub.com>
2019-10-12 21:33:00 -0400

    Commit for program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * @argc: Count of arguments
+ * @argv: Arguments passed
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
