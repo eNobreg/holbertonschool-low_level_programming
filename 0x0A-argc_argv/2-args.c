31a5978cf57242bf1b54ae35026916b33020b93e
eNobreg <ez@humdub.com>
2019-10-12 22:39:55 -0400

    betty compliant

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - main function
+ * @argc: Count of arguments passed from cmd line
+ * @argv: Values of arguments passed in
+ * Return: Always 0
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
