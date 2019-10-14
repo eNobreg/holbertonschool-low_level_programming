6548ad826cc9421697e0f2d66fdb5342141bbe11
eNobreg <ez@humdub.com>
2019-10-14 15:29:34 -0400

    add to github

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: Count of arguments
+ * @argv: Value of arguments
+ * Return: 1 or  0 depending
+ */
+int main(int argc, char *argv[])
+{
+	int coins[5] = {25, 10, 5, 2, 1};
+	int count = 0;
+	int cents;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argv[1] < 0)
+		printf("0\n");
+
+	cents = atoi(argv[1]);
+	for (int i = 0; i < 5;)
+	{
+		if (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+		else
+			i++;
+	}
+	printf("%d\n", count);
+	return (0);
+
+}
