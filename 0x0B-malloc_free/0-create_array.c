1724a3b03e2eb62592afd50df579a5ca4b6beffe
eNobreg <ez@humdub.com>
2019-10-15 12:44:57 -0400

    header and readme

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * create_array - creates an array of size @size
+ * @size: Size of arrray
+ * @c: Char to fill array wiht
+ * Return: Null or ar depending
+ */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *ar;
-	
 
-	if (size == 0) 
-		return(NULL);
-	else
-		ar = malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
 
 	while (i < size)
 	{
 		ar[i] = c;
-		i++;	
+		i++;
 	}
-
-	free(ar);
+	ar[i] = '\0';
 	return (ar);
 }
