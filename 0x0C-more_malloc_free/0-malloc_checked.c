9d5c9ad01c51dbd38df4a175910870b4aabcd192
eNobreg <ez@humdub.com>
2019-10-17 13:26:24 -0400

    task zero

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "holberton.h"
 #include <stdlib.h>
-
-
+/**
+ * malloc_checked - Malloc check
+ * @b: Size
+ * Return: Always ar
+ */
 void *malloc_checked(unsigned int b)
 {
 	int *ar;
