bdac7e85ddd9d6e9d90cc2f1e0734d1cb3ecd311
eNobreg <ez@humdub.com>
2019-10-17 13:48:57 -0400

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - range of array
+ * @min: Min value to print
+ * @max: Max to print
+ * Return: an int
+ */
+int *array_range(int min, int max)
+{
+	int add = 0;
+	int *arrInt;
+
+	arrInt = malloc((max - min + 1) * sizeof(int));
+	if (arrInt == NULL)
+		return (NULL);
+	if (min > max)
+		return (NULL);
+
+	for (; min <= max; min++)
+	{
+		arrInt[add] = min;
+		add++;
+	}
+	return (arrInt);
+}
