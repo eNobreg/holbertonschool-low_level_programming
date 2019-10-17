39d2ac5b769d69575cf752dc291360b01af1d400
eNobreg <ez@humdub.com>
2019-10-17 14:16:13 -0400

    changed array to char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int cnt = 0;
-	unsigned int *arr;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	arr = malloc(nmemb * sizeof(size));
 	if (arr == NULL)
 		return (NULL);
 
-	for (; cnt < (nmemb * size); cnt++)
+	for (; cnt < nmemb; cnt++)
 		arr[cnt] = 0;
 
 	return (arr);
