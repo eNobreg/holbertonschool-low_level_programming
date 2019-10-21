403dec4cfb53d4b12a84ac66bf0f278a2ca9d46b
eNobreg <ez@humdub.com>
2019-10-21 14:33:11 -0400

    fixed some leaks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,26 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * new_dog - new dog function
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Age of owner
+ * Return: New array
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int count = 0, i = 0, j = 0;
+	int count, i = 0, j = 0;
 	dog_t *dog2;
 
-	dog2 = (dog_t *)malloc(sizeof(struct dog));
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+
+	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
 		return (NULL);
-	
-	for (; name[i] != '\0'; i++)
-		;
-	for (; owner[j] != '\0'; j++)
-		;
 
 	dog2->name = malloc(sizeof(dog2->name) * i);
 	if (dog2->name == NULL)
@@ -22,6 +28,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog2);
 		return(NULL);
 	}
+
+	for (count = 0; count < i; count++)
+		dog2->name[count] = name[count];
+
+	/* Malloc for Owner  */
 	dog2->owner = malloc(sizeof(dog2->owner) * j);
 	if (dog2->owner == NULL)
 	{
@@ -32,8 +43,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog2->age = age;
 
-	for (count = 0; count < i; count++)
-		dog2->name[count] = name[count];
 	for (count = 0; count < j; count++)
 		dog2->owner[count] = owner[count];
 
