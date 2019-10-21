403e0794f7aad1a74119a461b205feebdc82c8cb
eNobreg <ez@humdub.com>
2019-10-21 14:38:52 -0400

    leak fix attempt

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,18 +22,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog2 == NULL)
 		return (NULL);
 
-	dog2->name = malloc(sizeof(dog2->name) * i);
+	dog2->name = malloc(sizeof(dog2->name) * (i + 1));
 	if (dog2->name == NULL)
 	{
 		free(dog2);
 		return(NULL);
 	}
 
-	for (count = 0; count < i; count++)
-		dog2->name[count] = name[count];
-
-	/* Malloc for Owner  */
-	dog2->owner = malloc(sizeof(dog2->owner) * j);
+	dog2->owner = malloc(sizeof(dog2->owner) * (j + 1));
 	if (dog2->owner == NULL)
 	{
 		free(dog2->name);
@@ -43,6 +39,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog2->age = age;
 
+	for (count = 0; count < i; count++)
+		dog2->name[count] = name[count];
 	for (count = 0; count < j; count++)
 		dog2->owner[count] = owner[count];
 
