a7b89db6cc99f4e25772106134e987d445892a62
eNobreg <ez@humdub.com>
2019-10-21 10:24:36 -0400

    Third task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function
+ * Return: Always exit success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
