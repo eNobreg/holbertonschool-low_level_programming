61baa864478d6217a284403c19dc9cc77a5c4ad1
eNobreg <ez@humdub.com>
2019-10-21 10:50:09 -0400

    dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains information about a dog
+ * @name: Name of the dog
+ * @age: Dog's age
+ * @owner: Owner's name
+ *
+ * Return: Never
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
