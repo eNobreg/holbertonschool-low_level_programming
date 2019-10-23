0325fe5af8819683acd1cffa302989d2f6ca125d
eNobreg <ez@humdub.com>
2019-10-23 14:59:54 -0400

    betty compliant

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,15 +10,13 @@
 int main(int argc, char *argv[])
 {
 	int (*func)(int, int);
-
+	char n = *argv[2];	
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	char n = *argv[2];
 
 	func = (get_op_func(argv[2]));
 
