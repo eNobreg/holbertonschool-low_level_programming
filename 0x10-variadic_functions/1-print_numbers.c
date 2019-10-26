6a69eab82cfe42908445ece742a9274f73a8ad33
eNobreg <ez@humdub.com>
2019-10-26 17:31:10 -0400

    betty compliant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: Seperator between entires
+ * @n: Number of entires
+ * Return: never
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list nums;
-	
+
 	va_start(nums, n);
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		if (i < n - 1 && separator != NULL)
 			printf("%d%s", va_arg(nums, int), separator);
-		else 
+		else
 			printf("%d", va_arg(nums, int));
 
 	}
+	printf("\n");
 	va_end(nums);
-	
+
 }
