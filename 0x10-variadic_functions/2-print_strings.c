c983f5ab5c250a85d29252a0327893e19aced0f5
eNobreg <ez@humdub.com>
2019-10-27 16:47:31 -0400

    betty compliant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,15 +2,22 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_strings - print a string
+ * @separator: Separator between strings
+ * @n: Number of arguments
+ * Return: never
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char * value;
+	char *value;
 	va_list strings;
-	
+
 	va_start(strings, n);
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		value = va_arg(strings, char *);
 		if (value == NULL)
@@ -18,11 +25,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1 && separator)
 			printf("%s%s", value, separator);
-		else 
+		else
 			printf("%s", value);
 
 	}
 	printf("\n");
 	va_end(strings);
-	
+
 }
