8e6aa84669cde139f461001d06c3868d0e888a80
eNobreg <ez@humdub.com>
2019-10-27 14:58:51 -0400

    created helper function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,30 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
+ * helper - a switch case
+ * @s: char to check case
+ * Return: 1 or 0
+ */
+
+int helper(char s)
+{
+	switch (s)
+	{
+		case 'c':
+			return (1);
+		case 'f':
+			return (1);
+		case 'i':
+			return (1);
+		case 's':
+			return (1);
+		default:
+			return (0);
+	}
+
+}
+
+/**
  * print_all - prints all entries
  * @format: Gets the format
  * Return: never
@@ -9,6 +33,7 @@
 void print_all(const char * const format, ...)
 {
 	int count2 = 0;
+	int help;
 	char *value;
 	va_list args;
 
@@ -37,7 +62,8 @@ void print_all(const char * const format, ...)
 				break;
 			}
 			count2++;
-			if (format[count2] != '\0')
+			help = helper(format[count2]);
+			if (format[count2] != '\0' && help == 1)
 				printf(", ");
 		}
 		printf("\n");
