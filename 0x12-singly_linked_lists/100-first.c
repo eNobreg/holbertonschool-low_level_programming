72185e2c7442d0059669383ee29219dcb7636abb
eNobreg <ezra.nobrega@outlook.com>
2019-11-01 11:41:08 -0400

    leak free

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * turtle - a turtle
+ * Return: Never
+ */
+void turtle(void) __attribute__ ((constructor));
+
+void turtle(void);
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n ");
+}
