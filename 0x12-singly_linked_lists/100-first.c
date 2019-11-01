ec6171d2f3dfb3311f339d0b9f33e21ab521197f
eNobreg <ezra.nobrega@outlook.com>
2019-11-01 11:43:38 -0400

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void turtle(void) __attribute__ ((constructor));
 void turtle(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n ");
+	printf("I bore my house upon my back!\n");
 }
