33badb4faffdfe3adda16604f1685ed25a432a4b
eNobreg <ezra.nobrega@outlook.com>
2019-11-05 15:56:40 -0500

    update header and new task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a list of ints
+ * @head: Nodes
+ * Return: Always current
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next, *current = *head;
+	
+	if (head == NULL)
+		return (NULL);
+
+	current = *head;
+	next = NULL;
+
+	while (current->next)
+	{
+		next = current->next;
+		current->next = next->next;
+		next->next = (*head);
+		*head = next;
+	}
+	return (*head);
+}
