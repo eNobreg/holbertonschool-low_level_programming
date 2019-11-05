69f79c73826b772db422c39d13759ff71b78ba0c
eNobreg <ezra.nobrega@outlook.com>
2019-11-05 13:55:07 -0500

    added helper function to print at the head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int cnt = 0;
 
 	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL || new == NULL)
 		return (NULL);
