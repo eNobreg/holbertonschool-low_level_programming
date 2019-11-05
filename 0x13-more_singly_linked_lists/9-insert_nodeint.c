182deb2c1d58dc52b89cb4490cc75177b41c0b71
eNobreg <ezra.nobrega@outlook.com>
2019-11-05 10:22:47 -0500

    adding a node at an index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	new->n = n;
-	if (*head == NULL)
+	if (*head == NULL || new == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
