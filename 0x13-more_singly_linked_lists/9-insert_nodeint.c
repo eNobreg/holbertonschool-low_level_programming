88e75914a7cf2887560db62ddedc66b0431d0998
eNobreg <ezra.nobrega@outlook.com>
2019-11-05 13:51:02 -0500

    added helper function to print at the head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+
+listint_t *add_nodeint(listint_t **head, const int n);
 /**
  * insert_nodeint_at_index - inserts a node at index
  * @head: Address of the node of the singly linked list
@@ -10,29 +12,54 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *current;
+	unsigned int cnt = 0;
+
+	new->n = n;
+
+	if (*head == NULL || new == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		add_nodeint(head, n);
+	}
+
+	current = *head;
+
+	while (current)
+	{
+		if (idx - 1 == cnt)
+		{
+			new->next = current->next;
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+		cnt++;
+	}
+	free(new);
+	return (NULL);
+}
+/**
+ * add_nodeint - Adds a node at the head
+ * @head: Current node
+ * @n: data for node
+ * Return: New
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-
+	new->next = *head;
+	*head = new;
 	if (*head == NULL)
-		return (new);
-	if (idx == 0)
 	{
-		new->next = *head;
-
-		return (new);
+		return (NULL);
 	}
 
-	current = *head;
-
-	while (idx - 1 > 0)
-	{
-		current = current->next;
-		idx--;
-	}
-	new->next = current->next;
-	current->next = new;
 	return (new);
 }
