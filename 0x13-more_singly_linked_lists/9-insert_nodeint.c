2bffd59cf0edea50750b320af57af8672cb75605
eNobreg <ezra.nobrega@outlook.com>
2019-11-05 15:13:47 -0500

    fix a return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = current->next;
 			current->next = new;
-			return (current);
+			return (new);
 		}
 		cnt++;
 		current = current->next;
