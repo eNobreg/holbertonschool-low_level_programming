eb0fc30578a165b3ab1726c555d19fc872a18c3e
eNobreg <ezra.nobrega@outlook.com>
2019-11-07 15:35:39 -0500

    new header and file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * flip_bits - amount of bits to flip
+ * @n: Value 1
+ * @m: Value 2
+ * Return: Result
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int value = n ^ m;
+	int result = 0;
+
+	while (value > 0)
+	{
+		if (value & 1)
+			result++;
+		value = value >> 1;
+	}
+
+
+	return (result);
+}
