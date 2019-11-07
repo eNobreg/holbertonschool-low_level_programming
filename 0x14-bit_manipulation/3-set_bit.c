1adfab3f5292c950082371e60638d7c6b193212c
eNobreg <ezra.nobrega@outlook.com>
2019-11-07 15:14:10 -0500

    set a bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * set_bit - gets a bit
+ * @n: Value
+ * @index: Index to check vlaue at
+ * Return: 1 or 0
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index < sizeof(n) * 8)
+	{
+		*n = *n | (1 << index);
+		return (-1);
+	}
+
+	return (-1);
+}
