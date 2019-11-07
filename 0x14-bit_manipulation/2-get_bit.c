bb10c1f1eda2ff5e073b33bfc8ff4ef6cba7e25b
eNobreg <ezra.nobrega@outlook.com>
2019-11-07 14:55:14 -0500

    update header and task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * get_bit - gets a bit
+ * @n: Value
+ * @index: Index to check vlaue at
+ * Return: 1 or 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	mask <<= index;
+
+	if (mask & n)
+		return (1);
+	else
+		return (0);
+}
