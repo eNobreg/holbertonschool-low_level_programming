aa4d01f3e780260635447198fb7da695bb8ce3e3
eNobreg <ezra.nobrega@outlook.com>
2019-11-12 15:09:47 -0500

    updates

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,7 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -11,5 +10,6 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
