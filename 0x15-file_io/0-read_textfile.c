63bbcd62dbe520ff1233322eeea550010b531cc2
eNobreg <ezra.nobrega@outlook.com>
2019-11-12 13:37:29 -0500

    Made updates for better

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
+/**
+ * read_textfile - Reads a text file
+ * @filename: File to read
+ * @letters: Letters in the file
+ * Return: c
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	char *buffer;
 	ssize_t c, d, e;
 
-	buffer = malloc(sizeof(char)* letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (-1);
 
@@ -21,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	file = open(filename, O_RDONLY);
 	if (file == -1)
-		return(0);
+		return (0);
 
 	c = read(file, buffer, letters);
 	if (c == -1)
@@ -30,10 +29,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	d = write(STDOUT_FILENO, buffer, c);
 	if (d == -1)
 		return (0);
-	
+
 	e = close(file);
 	if (e == -1)
 		return (0);
-	
-	return(c);
+
+	return (c);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
