ade435fba55c3b95fdc99de9f9d3d775274bf601
eNobreg <ezra.nobrega@outlook.com>
2019-12-14 12:07:16 -0500

    update

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a node
+ * @head: Main passed in
+ * @n: number to coy
+ * Return: Always new
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+
+	return (new);
+}
