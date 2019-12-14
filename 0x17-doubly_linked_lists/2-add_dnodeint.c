86d1ae8547ce5a3039c016c74d3777adf60b470c
eNobreg <ezra.nobrega@outlook.com>
2019-12-14 14:08:27 -0500

    attempt

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = current;
-	current->prev = new;
+	new->next = *head;
+	if (*head != NULL)
+		current->prev = new;
 	*head = new;
 	if (*head == NULL)
 		return (NULL);
