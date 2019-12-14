ae4a88137724e790c56924722047c8b8dfbff912
eNobreg <ezra.nobrega@outlook.com>
2019-12-14 13:57:26 -0500

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,11 +8,12 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-
+	dlistint_t *current = *head;
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	new->next = *head;
+	new->next = current;
 	new->prev = NULL;
 	*head = new;
 
