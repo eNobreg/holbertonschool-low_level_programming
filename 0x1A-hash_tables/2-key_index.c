489eea09ca1c3d11f97b5f5418bc0fc0ba4ba20a
eNobreg <ezra.nobrega@outlook.com>
2020-01-18 12:30:09 -0500

    [Initial]

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,7 @@
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	const unsigned long int new_key = hash_djb2(key) % size;
+	return (new_key);
+}
