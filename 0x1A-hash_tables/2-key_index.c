ce9c528df54f54e13c0217da68c3c81a703f3f19
eNobreg <ezra.nobrega@outlook.com>
2020-01-18 12:33:21 -0500

    [Fix] Betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,13 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - Sets the key index
+ * @key: Key for the dictionary
+ * @size: Size of the array
+ * Return: The new key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	const unsigned long int new_key = hash_djb2(key) % size;
+
 	return (new_key);
 }
