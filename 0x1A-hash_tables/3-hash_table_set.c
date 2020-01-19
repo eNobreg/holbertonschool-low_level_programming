fde4d925a1f04ff2862904076cc454e4a7a4a493
eNobreg <ezra.nobrega@outlook.com>
2020-01-18 23:13:35 -0500

    [Change] To free memory and update prototype

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,14 @@ hash_node_t *pair(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new_slot = malloc(sizeof(hash_node_t));
+	hash_node_t *new_slot = malloc(sizeof(hash_node_t)), *temp = NULL;
 
 	if (!key || !value || !ht || strlen(key) == 0)
 		return (0);
 
 	if (new_slot == NULL)
 		return (0);
+	temp = new_slot;
 	index = key_index((const unsigned char *)key, ht->size);
 
 	new_slot = pair(key, value);
@@ -27,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_slot->next = ht->array[index];
 	ht->array[index] = new_slot;
+	free(temp);
 	return (1);
 }
 /**
@@ -37,15 +39,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  */
 hash_node_t *pair(const char *key, const char *value)
 {
-	hash_node_t *new = malloc(sizeof(hash_node_t) * 1);
+	hash_node_t *new = malloc(sizeof(hash_node_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
-	if (new->key == NULL)
+	if (new->key == NULL) 
+	{
+		free(new);
 		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return(NULL);
+	}
+	new->next = NULL;
 	return (new);
 }
