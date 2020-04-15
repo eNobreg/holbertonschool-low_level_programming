94c14d27abd569c2e9a99e5762e8fa61f363447e
eNobreg <ezra.nobrega@outlook.com>
2020-04-15 15:21:04 -0400

    [Fix] < to <=

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,9 @@ int jump_search(int *array, size_t size, int value)
 	size_t start = 0, i = 0;
 	size_t jump = 3;
 
+	if (array == NULL)
+		return (-1);
+
 	if (array[size - 1] == value)
 		return (size - 1);
 
@@ -22,7 +25,7 @@ int jump_search(int *array, size_t size, int value)
 		if (array[start] == value)
 			return (start);
 
-		if (value > array[start] && value < array[start + 3])
+		if (value > array[start] && value <= array[start + 3])
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n", start, jump);
 			for (i = start; i <= jump; i++)
